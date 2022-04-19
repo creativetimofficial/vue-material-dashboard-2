79e8cb07a457f9a57fe3f6e1bb432fc1fdc8f3ad
Maisam Afshar <maisam.afshar@gmail.com>
2022-04-19 14:14:09 +0430

    add reportsBarChart component

---
+++ src/examples/Charts/ReportsBarChart.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="chart">
+    <canvas :id="id" class="chart-canvas" :height="height"></canvas>
+  </div>
+</template>
+<script>
+import Chart from "chart.js/auto";
+
+export default {
+  name: "ReportsBarChart",
+  props: {
+    id: {
+      type: String,
+      default: "bar-chart",
+    },
+    height: {
+      type: [Number, String],
+      default: "170",
+    },
+    chart: {
+      type: Object,
+      required: true,
+      labels: Array,
+      datasets: {
+        type: Object,
+        label: String,
+        data: Array,
+      },
+    },
+  },
+
+  mounted() {
+    var ctx = document.getElementById(this.id).getContext("2d");
+
+    let chartStatus = Chart.getChart(this.id);
+    if (chartStatus != undefined) {
+      chartStatus.destroy();
+    }
+
+    new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: this.chart.labels,
+        datasets: [
+          {
+            label: this.chart.datasets.label,
+            tension: 0.4,
+            borderWidth: 0,
+            borderRadius: 4,
+            borderSkipped: false,
+            backgroundColor: "rgba(255, 255, 255, .8)",
+            data: this.chart.datasets.data,
+            maxBarThickness: 6,
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          legend: {
+            display: false,
+          },
+        },
+        interaction: {
+          intersect: false,
+          mode: "index",
+        },
+        scales: {
+          y: {
+            grid: {
+              drawBorder: false,
+              display: true,
+              drawOnChartArea: true,
+              drawTicks: false,
+              borderDash: [5, 5],
+              color: "rgba(255, 255, 255, .2)",
+            },
+            ticks: {
+              suggestedMin: 0,
+              suggestedMax: 500,
+              beginAtZero: true,
+              padding: 10,
+              font: {
+                size: 14,
+                weight: 300,
+                family: "Roboto",
+                style: "normal",
+                lineHeight: 2,
+              },
+              color: "#fff",
+            },
+          },
+          x: {
+            grid: {
+              drawBorder: false,
+              display: true,
+              drawOnChartArea: true,
+              drawTicks: false,
+              borderDash: [5, 5],
+              color: "rgba(255, 255, 255, .2)",
+            },
+            ticks: {
+              display: true,
+              color: "#f8f9fa",
+              padding: 10,
+              font: {
+                size: 14,
+                weight: 300,
+                family: "Roboto",
+                style: "normal",
+                lineHeight: 2,
+              },
+            },
+          },
+        },
+      },
+    });
+  },
+};
+</script>
