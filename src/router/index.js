65925b9c95ba1efcebf1807b516fee2060ea24ee
sajadevo <nawabisajadahmad@gmail.com>
2022-06-14 11:50:07 +0430

    Ready for deploy

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Dashboard from "../views/Dashboard.vue";
 import Tables from "../views/Tables.vue";
 import Billing from "../views/Billing.vue";
@@ -12,54 +12,54 @@ const routes = [
   {
     path: "/",
     name: "/",
-    redirect: "/dashboard",
+    redirect: "/dashboard"
   },
   {
     path: "/dashboard",
     name: "Dashboard",
-    component: Dashboard,
+    component: Dashboard
   },
   {
     path: "/tables",
     name: "Tables",
-    component: Tables,
+    component: Tables
   },
   {
     path: "/billing",
     name: "Billing",
-    component: Billing,
+    component: Billing
   },
   {
     path: "/rtl-page",
     name: "RTL",
-    component: RTL,
+    component: RTL
   },
   {
     path: "/notifications",
     name: "Notifications",
-    component: Notifications,
+    component: Notifications
   },
   {
     path: "/profile",
     name: "Profile",
-    component: Profile,
+    component: Profile
   },
   {
     path: "/sign-in",
     name: "SignIn",
-    component: SignIn,
+    component: SignIn
   },
   {
     path: "/sign-up",
     name: "SignUp",
-    component: SignUp,
-  },
+    component: SignUp
+  }
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   routes,
-  linkActiveClass: "active",
+  linkActiveClass: "active"
 });
 
 export default router;
